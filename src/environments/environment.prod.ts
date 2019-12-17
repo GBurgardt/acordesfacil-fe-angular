1bf020f979ef66ff50f59fbd51c6586336b7a233
german <german@ahoradoctor.com>
2019-12-17 19:11:24 +0000

    fa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   // WS_URL: 'http://localhost:3000'
-  WS_URL: 'https://acordesfacil.tk:3000'
+  // WS_URL: 'https://acordesfacil.tk:3000'
+	// WS_URL: 'https://ec2-3-19-27-20.us-east-2.compute.amazonaws.com:3000'
+	WS_URL: 'https://acordesfacil.crabdance.com:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
     production: false,
-    WS_URL: 'http://localhost:3000'
+    // WS_URL: 'http://localhost:3000'
     // WS_URL: 'https://acordesfacil.tk:3000'
+    // WS_URL: 'https://3.19.226.37:3000'
+    // WS_URL: 'http://ec2-3-19-226-37.us-east-2.compute.amazonaws.com:3000'
+    WS_URL: 'https://acordesfacil.crabdance.com:3000'
+    // WS_URL: 'https://ip-172-31-39-253:3000'
 };
